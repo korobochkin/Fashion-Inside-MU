56d0887aed90b8d089bd7a141d754672521a1c11
Kolya Korobochkin <me@korobochkin.com>
2014-11-08 18:56:23 +0300

    Соц кнопки от Константина Ковшенина

---
+++ fashion-inside/yandex-share/plugin.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Удаляем соц кнопки после контента, которые добавляет плагин
+ */
+function fashion_inside_yandex_share_init () {
+	remove_filter ('the_content', array ($GLOBALS['yandex_share_plugin'], 'the_content'));
+}
+add_action ('init', 'fashion_inside_yandex_share_init', 20);
+
+/*
+ * И добавляем соц кнопки в нужное место
+ */
+function fashion_inside_add_social_buttons ($content) {
+	if (!is_preview ()) {
+		if (isset ($GLOBALS['yandex_share_plugin'])) {
+			$social_btns = $GLOBALS['yandex_share_plugin']->the_content ('');
+			$social_btns = '<div class="ya-share-container"><div class="ya-share-container-inner">' . $social_btns . '</div></div>';
+			return $social_btns . $content;
+		}
+	}
+	return $content;
+}
+add_filter ('selena_entry_meta_comments_counter_add_something', 'fashion_inside_add_social_buttons', 99);
+?>
--- load.php
+++ load.php
@@ -5,6 +5,9 @@ require_once (WPMU_PLUGIN_DIR . '/fashion-inside/wordpress-logo/plugin.php');
 // Delete Seo by Yoast Stuff
 require_once (WPMU_PLUGIN_DIR . '/fashion-inside/seo-by-yoast/plugin.php');
 
+// Yandex Share Social buttons
+require WPMU_PLUGIN_DIR . '/fashion-inside/yandex-share/plugin.php';
+
 // Themes
 // Selena
 require WPMU_PLUGIN_DIR . '/fashion-inside/themes/selena/plugin.php';
