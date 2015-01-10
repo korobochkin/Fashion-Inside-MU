5247cfbc4dcf790078c976d3a495ec394ccad48b
Kolya Korobochkin <me@korobochkin.com>
2015-01-10 17:35:18 +0300

    Apple touch icon для старых iPhone.

--- plugin/fashion-inside/wp-head/plugin.php
+++ plugin/fashion-inside/wp-head/plugin.php
@@ -3,6 +3,7 @@ function fashion_inside_wp_head_icons () {
 ?>
 <link rel="shortcut icon" href="/favicon.ico">
 <meta name="apple-mobile-web-app-title" content="<?php echo esc_attr (get_bloginfo ('name')); ?>">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo site_url ('apple-touch-icon-60x60-precomposed.png'); ?>">
 <link rel="apple-touch-icon" sizes="76x76" href="<?php echo site_url ('apple-touch-icon-76x76-precomposed.png'); ?>">
 <link rel="apple-touch-icon" sizes="120x120" href="<?php echo site_url ('apple-touch-icon-120x120-precomposed.png'); ?>">
 <link rel="apple-touch-icon" sizes="152x152" href="<?php echo site_url ('apple-touch-icon-152x152-precomposed.png'); ?>">
