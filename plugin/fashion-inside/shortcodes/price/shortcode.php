1f4d8dd3925cb8f07060792a7c7620fc2b75ac37
Kolya Korobochkin <me@korobochkin.com>
2015-03-05 23:43:15 +0300

    Caption для "ценника" и правильная обработка дробных сумм

--- plugin/fashion-inside/shortcodes/price/shortcode.php
+++ plugin/fashion-inside/shortcodes/price/shortcode.php
@@ -1,15 +1,17 @@
 <?php
-function fashion_inside_shortcode_price ($attrs) {
+function fashion_inside_shortcode_price ($attrs, $content = null) {
 	$html = '';
 
 	if ($attrs['value']) {
+
 		$html = '<span class="price-value">' . number_format (
 				(float)$attrs['value'],
-				2,
+				((float)$attrs['value'] == (int)$attrs['value'] ? 0 : 2),
 				_x ('.', 'The separator for the decimal point.', 'fashion_inside'),
 				_x (',', 'The thousands separator.', 'fashion_inside')
 			) . '</span>';
 
+		// Надо узнать можем ли мы так писать, т к нужной ячейки может не быть
 		if ($attrs['currency']) {
 			switch ($attrs['currency']) {
 				case "RUB":
@@ -26,14 +28,8 @@ function fashion_inside_shortcode_price ($attrs) {
 
 		$html = '<span class="price-content">' . $html . '</span>';
 
-		// Надо узнать можем ли мы так писать, т к нужной ячейки может не быть
-		if ($attrs['caption']) {
-			if ($attrs['caption_link']) {
-				$html .= '<span class="price-caption"><a href="' . $attrs['caption_link'] . '">' . $attrs['caption'] . '</a></span>';
-			}
-			else {
-				$html .= '<span class="price-caption">' . $attrs['caption'] . '</span>';
-			}
+		if ($content) {
+			$html .= '<span class="price-caption">' . $content .'</span>';
 		}
 
 		$html = '<span class="price">' . $html . '</span>';
