8486932b1eb607b0e48c78f4d34efa086caf00a8
Kolya Korobochkin <me@korobochkin.com>
2015-03-06 21:27:41 +0300

    Ззнак российского рубля

--- plugin/fashion-inside/shortcodes/price/shortcode.php
+++ plugin/fashion-inside/shortcodes/price/shortcode.php
@@ -15,7 +15,7 @@ function fashion_inside_shortcode_price ($attrs, $content = null) {
 		if ($attrs['currency']) {
 			switch ($attrs['currency']) {
 				case "RUB":
-					$currency_sign = _x ('Rub', 'The Russian Ruble mark', 'fashion_inside');
+					$currency_sign = _x ('₽', 'The Russian Ruble mark', 'fashion_inside');
 					break;
 
 				case "BYR":
