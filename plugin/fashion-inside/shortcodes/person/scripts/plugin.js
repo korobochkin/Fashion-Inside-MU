7b171373abde59a3fdbadc8190ba2de5dc8ae62f
Kolya Korobochkin <me@korobochkin.com>
2015-03-08 01:37:13 +0300

    JS на будущее

--- plugin/fashion-inside/shortcodes/person/scripts/plugin.js
+++ plugin/fashion-inside/shortcodes/person/scripts/plugin.js
@@ -1,5 +1,11 @@
 tinymce.PluginManager.add('wpperson', function( editor ) {
 
+	function replacePersonShortcodes ( content ) {
+		return content.replace( /\[person([^\]]*)\]/g, function( match ) {
+			return html( 'wp-gallery', match );
+		});
+	}
+
 	function editPerson( node ) {
 
 	}
@@ -7,6 +13,14 @@ tinymce.PluginManager.add('wpperson', function( editor ) {
 	// Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('...');
 	editor.addCommand( 'WP_Person', function() {
 		editPerson( editor.selection.getNode() );
+	});
+
+
+	editor.on( 'BeforeSetContent', function( event ) {
+		// 'wpview' handles the gallery shortcode when present
+		if ( ! editor.plugins.wpperson || typeof wp === 'undefined' || ! wp.mce ) {
+			event.content = replacePersonShortcodes( event.content );
+		}
 	});
 
 });
