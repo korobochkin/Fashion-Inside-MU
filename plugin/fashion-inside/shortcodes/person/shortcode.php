6bff668259f6e2e369babf7d635e14a5d51ca835
Kolya Korobochkin <me@korobochkin.com>
2015-03-09 01:45:19 +0300

    Упрощаем шорткод. Ни к чему писать очередной ИИ

--- plugin/fashion-inside/shortcodes/person/shortcode.php
+++ plugin/fashion-inside/shortcodes/person/shortcode.php
@@ -1,7 +1,8 @@
 <?php
 function fashion_inside_shortcode_person ($attrs, $content = null) {
+	//$content = $content;
 	$content = do_shortcode ($content);
-
+	//return;
 	if ($content) {
 		 return '<div class="person_card"><div class="media">' . $content . '</div></div>';
 	}
@@ -24,11 +25,32 @@ function fashion_inside_shortcode_person_portrait ($attrs, $content = null) {
 
 	return $html;
 }
-add_shortcode ('person_portrait', 'fashion_inside_shortcode_person_portrait');
+//add_shortcode ('person_portrait', 'fashion_inside_shortcode_person_portrait');
 
 function fashion_inside_shortcode_person_about ($attrs, $content = null) {
 	if ($content) {
-		return '<div class="media-body"><div class="person_about">' . $content . '</div></div>';
+		return '<div class="media-body"><div class="person_about">' . do_shortcode ($content) . '</div></div>';
 	}
 }
-add_shortcode ('person_about', 'fashion_inside_shortcode_person_about');
+//add_shortcode ('person_about', 'fashion_inside_shortcode_person_about');
+
+function fashion_inside_shortcode_person_title ($attrs, $content = null) {
+	if ($content) {
+		return '<h3>' . $content . '</h3>';
+	}
+}
+//add_shortcode ('person_title', 'fashion_inside_shortcode_person_title');
+
+function fashion_inside_shortcode_person_position ($attrs, $content = null) {
+	if ($content) {
+		return '<p class="position">' . $content . '</p>';
+	}
+}
+//add_shortcode ('person_position', 'fashion_inside_shortcode_person_position');
+
+function fashion_inside_shortcode_person_description ($attrs, $content = null) {
+	if ($content) {
+		return '<p class="description">' . $content . '</p>';
+	}
+}
+//add_shortcode ('person_desc', 'fashion_inside_shortcode_person_description');
