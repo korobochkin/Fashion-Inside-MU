2f6fdce293e660780b9ecae70d49df0e6bf827d7
Kolya Korobochkin <me@korobochkin.com>
2015-03-09 02:15:28 +0300

    Причесываем шорткод

--- plugin/fashion-inside/shortcodes/person/shortcode.php
+++ plugin/fashion-inside/shortcodes/person/shortcode.php
@@ -1,56 +1,32 @@
 <?php
 function fashion_inside_shortcode_person ($attrs, $content = null) {
-	//$content = $content;
-	$content = do_shortcode ($content);
-	//return;
-	if ($content) {
-		 return '<div class="person_card"><div class="media">' . $content . '</div></div>';
+
+	$attrs = shortcode_atts (
+		array (
+			'avatar_url' => '',
+			'name'       => _x ('Anonymous', 'Anonymous person name', 'fashion_inside'),
+			'position'   => ''
+		),
+		$attrs,
+		'person'
+	);
+
+	if ($attrs['avatar_url']) {
+		$html = '<div class="media-left"><div class="person_portrait"><img src="' . $attrs['avatar_url'] . '" width="150" height="150" class="img-responsive" alt="' . $attrs['name'] . '"></div></div>';
+
+
+		$html .= '<div class="media-body"><div class="person_about"><h3>' . $attrs['name'] .'</h3>';
+
+		if ($attrs['position']) {
+			$html .= '<p class="position">' . $attrs['position'] . '</p>';
+		}
+		if ($content) {
+			$html .= '<p class="description">' . $content . '</p>';
+		}
+
+		$html .= '</div></div>';
+
+		return $html;
 	}
 }
 add_shortcode ('person', 'fashion_inside_shortcode_person');
-
-function fashion_inside_shortcode_person_portrait ($attrs, $content = null) {
-	$html = '<div class="media-left"><div class="person_portrait">';
-
-	$content = trim ($content);
-
-	if ($content) {
-		$html .= $content;
-	}
-	else {
-		$html .= '<img src="/human-face.png" width="150" height="150">';
-	}
-
-	$html .= '</div></div>';
-
-	return $html;
-}
-//add_shortcode ('person_portrait', 'fashion_inside_shortcode_person_portrait');
-
-function fashion_inside_shortcode_person_about ($attrs, $content = null) {
-	if ($content) {
-		return '<div class="media-body"><div class="person_about">' . do_shortcode ($content) . '</div></div>';
-	}
-}
-//add_shortcode ('person_about', 'fashion_inside_shortcode_person_about');
-
-function fashion_inside_shortcode_person_title ($attrs, $content = null) {
-	if ($content) {
-		return '<h3>' . $content . '</h3>';
-	}
-}
-//add_shortcode ('person_title', 'fashion_inside_shortcode_person_title');
-
-function fashion_inside_shortcode_person_position ($attrs, $content = null) {
-	if ($content) {
-		return '<p class="position">' . $content . '</p>';
-	}
-}
-//add_shortcode ('person_position', 'fashion_inside_shortcode_person_position');
-
-function fashion_inside_shortcode_person_description ($attrs, $content = null) {
-	if ($content) {
-		return '<p class="description">' . $content . '</p>';
-	}
-}
-//add_shortcode ('person_desc', 'fashion_inside_shortcode_person_description');
