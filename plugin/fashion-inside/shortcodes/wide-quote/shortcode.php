450527484c6df49772c056df88997cc477a16000
Kolya Korobochkin <me@korobochkin.com>
2015-03-15 02:55:50 +0300

    Допилен README и проверка атрибутов шорткода.

--- plugin/fashion-inside/shortcodes/wide-quote/shortcode.php
+++ plugin/fashion-inside/shortcodes/wide-quote/shortcode.php
@@ -7,17 +7,20 @@ function fashion_inside_shortcode_wide_quote ($attrs, $content) {
 		$attrs = shortcode_atts (
 			array (
 				'author' => null,
-				'color'  => 'light',
-				'preset'  => '',
+				'color'  => 'dark',
+				'preset'  => null,
 				'bgscheme' => 'color',
-				'ratio'  => 'none',
+				'ratio'  => null,
 				'vertical_align' => 'bottom',
-				'background_image' => '',
-				'background_color' => ''
+				'background_image' => null,
+				'background_color' => null
 			),
 			$attrs,
 			'wide-quote'
 		);
+
+
+		// Текст цитаты и имя автора
 		$html = '<div class="ratio"></div><div class="wide-quote-text"><div class="row"><div class="col-xs-3"><div class="wide-quote-quote" draggable="false" selectable="false">“</div></div><div class="col-xs-9"><p>' . $content . '</p>';
 
 		if ($attrs['author']) {
@@ -34,10 +37,12 @@ function fashion_inside_shortcode_wide_quote ($attrs, $content) {
 		}
 		else {
 			$classes .= ' wide-quote-' . $attrs['color'];
+			$classes .= ' wide-quote-' . $attrs['bgscheme'];
 		}
-		$classes .= ' wide-quote-' . $attrs['bgscheme'];
-		$classes .= ' wide-quote-' . $attrs['ratio'];
-		$classes .= ' wide-quote-' . $attrs['vertical_align'];
+		if ($attrs['ratio']) {
+			$classes .= ' wide-quote-' . $attrs['ratio'];
+			$classes .= ' wide-quote-' . $attrs['vertical_align'];
+		}
 		$classes = 'class="' . $classes . '" ';
 
 
@@ -46,10 +51,10 @@ function fashion_inside_shortcode_wide_quote ($attrs, $content) {
 		if ($attrs['background_color']) {
 			$styles = 'background-color:' . $attrs['background_color'] . ';';
 		}
-		if($attrs['background_image']) {
+		if ($attrs['background_image']) {
 			$styles .= 'background-image: url(\'' . $attrs['background_image'] . '\');';
 		}
-		if($styles) {
+		if ($styles) {
 			$styles = 'style="' . $styles . '"';
 		}
 
